2a20537686248fa222a37968ed38c080a597c4c9
Lin <LYS86@users.noreply.github.com>
2025-03-01 11:07:10 +0800

    更新LeakCanary版本

--- app/src/main/java/org/autojs/autojs/App.kt
+++ app/src/main/java/org/autojs/autojs/App.kt
@@ -12,7 +12,6 @@ import androidx.multidex.MultiDexApplication
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
-import com.squareup.leakcanary.LeakCanary
 import com.stardust.app.GlobalAppContext
 import com.stardust.autojs.core.ui.inflater.ImageLoader
 import com.stardust.autojs.core.ui.inflater.util.Drawables
@@ -59,13 +58,6 @@ class App : MultiDexApplication() {
 
         crashHandler.setBuglyHandler(Thread.getDefaultUncaughtExceptionHandler())
         Thread.setDefaultUncaughtExceptionHandler(crashHandler)
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        //LeakCanary.install(this);
-
     }
 
     private fun init() {
