fadcc91aeed0c431fcac88ab7d3cd5ef1a1063b1
Lin <LYS86@users.noreply.github.com>
2025-03-01 10:40:06 +0800

    AboutActivity 迁移到View Binding

--- app/src/main/java/org/autojs/autojs/ui/settings/AboutActivity.java
+++ app/src/main/java/org/autojs/autojs/ui/settings/AboutActivity.java
@@ -1,102 +1,88 @@
 package org.autojs.autojs.ui.settings;
 
 import android.annotation.SuppressLint;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import org.autojs.autojs.tool.IntentTool;
-import org.autojs.autojs.ui.BaseActivity;
-import org.autojs.autojs.theme.dialog.ThemeColorMaterialDialogBuilder;
 import com.stardust.util.IntentUtil;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import org.autojs.autojs.BuildConfig;
 import org.autojs.autojs.R;
+import org.autojs.autojs.databinding.ActivityAboutBinding;
+import org.autojs.autojs.databinding.ActivityAboutItemsBinding;
+import org.autojs.autojs.theme.dialog.ThemeColorMaterialDialogBuilder;
+import org.autojs.autojs.tool.IntentTool;
+import org.autojs.autojs.ui.BaseActivity;
 
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
-
-/**
- * Created by Stardust on 2017/2/2.
- */
-@EActivity(R.layout.activity_about)
 public class AboutActivity extends BaseActivity {
 
     private static final String TAG = "AboutActivity";
-    @ViewById(R.id.version)
-    TextView mVersion;
-
+    private ActivityAboutBinding mainBinding;
+    private ActivityAboutItemsBinding itemsBinding;
     private int mLolClickCount = 0;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityAboutBinding.inflate(getLayoutInflater());
+        View aboutItemsView = mainBinding.getRoot().findViewById(R.id.about_items_root);
+        itemsBinding = ActivityAboutItemsBinding.bind(aboutItemsView);
+        setContentView(mainBinding.getRoot());
 
-    @AfterViews
-    void setUpViews() {
-        setVersionName();
-        setToolbarAsBack(getString(R.string.text_about));
+        setUpViews();
+        setupClickListeners();
     }
 
     @SuppressLint("SetTextI18n")
-    private void setVersionName() {
-        mVersion.setText("Version " + BuildConfig.VERSION_NAME);
+    private void setUpViews() {
+        mainBinding.version.setText("Version " + BuildConfig.VERSION_NAME);
+        setToolbarAsBack(getString(R.string.text_about));
     }
 
-    @Click(R.id.github)
-    void openGitHub() {
+    private void setupClickListeners() {
+        itemsBinding.github.setOnClickListener(v -> openGitHub());
+        itemsBinding.qq.setOnClickListener(v -> openQQToChatWithMe());
+        itemsBinding.email.setOnClickListener(v -> openEmailToSendMe());
+
+        mainBinding.share.setOnClickListener(v -> share());
+        mainBinding.icon.setOnClickListener(v -> lol());
+    }
+
+    private void openGitHub() {
         IntentTool.browse(this, getString(R.string.my_github));
     }
 
-    @Click(R.id.qq)
-    void openQQToChatWithMe() {
+    private void openQQToChatWithMe() {
         String qq = getString(R.string.qq);
         if (!IntentUtil.chatWithQQ(this, qq)) {
             Toast.makeText(this, R.string.text_mobile_qq_not_installed, Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Click(R.id.email)
-    void openEmailToSendMe() {
+    private void openEmailToSendMe() {
         String email = getString(R.string.email);
         IntentUtil.sendMailTo(this, email);
     }
 
-
-    @Click(R.id.share)
-    void share() {
+    private void share() {
         IntentUtil.shareText(this, getString(R.string.share_app));
     }
 
-    @Click(R.id.icon)
-    void lol() {
+    private void lol() {
         mLolClickCount++;
-        //Toast.makeText(this, R.string.text_lll, Toast.LENGTH_LONG).show();
         if (mLolClickCount >= 5) {
             crashTest();
-            //showEasterEgg();
         }
-    }
-
-    private void showEasterEgg() {
-        new MaterialDialog.Builder(this)
-                .customView(R.layout.paint_layout, false)
-                .show();
     }
 
     private void crashTest() {
         new ThemeColorMaterialDialogBuilder(this)
                 .title("Crash Test")
                 .positiveText("Crash")
-                .onPositive((dialog, which) -> {
-                    CrashReport.testJavaCrash();
-                }).show();
+                .onPositive((dialog, which) -> CrashReport.testJavaCrash())
+                .show();
     }
-
-    @Click(R.id.developer)
-    void hhh() {
-        Toast.makeText(this, R.string.text_it_is_the_developer_of_app, Toast.LENGTH_LONG).show();
-    }
-
 
 }
