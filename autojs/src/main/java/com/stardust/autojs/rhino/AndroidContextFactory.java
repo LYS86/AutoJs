7c9ce33e1538168845daf1408c5686a999d137bf
Lin <LYS86@users.noreply.github.com>
2025-03-01 10:26:09 +0800

    升级Rhino 1.7.15

--- autojs/src/main/java/com/stardust/autojs/rhino/AndroidContextFactory.java
+++ autojs/src/main/java/com/stardust/autojs/rhino/AndroidContextFactory.java
@@ -1,6 +1,8 @@
 package com.stardust.autojs.rhino;
 
 
+import android.util.Log;
+
 import com.stardust.autojs.runtime.exception.ScriptInterruptedException;
 
 import org.mozilla.javascript.Context;
@@ -22,6 +24,12 @@ public class AndroidContextFactory extends ShellContextFactory {
      */
     public AndroidContextFactory(File cacheDirectory) {
         this.cacheDirectory = cacheDirectory;
+        /*
+         * 修复 Rhino 1.7.15 的 Context.emptyArgs 初始化问题。
+         * 通过提前访问该字段触发静态初始化，避免后续使用时为 null。
+         * 问题详情：https://github.com/mozilla/rhino/issues/1793
+         */
+        Log.d("AndroidContextFactory", "Init Context.emptyArgs: " + Context.emptyArgs);
         initApplicationClassLoader(createClassLoader(AndroidContextFactory.class.getClassLoader()));
     }
 
@@ -50,5 +58,11 @@ public class AndroidContextFactory extends ShellContextFactory {
         return cx;
     }
 
-
+    @Override
+    public boolean hasFeature(Context cx, int featureIndex) {
+        if (featureIndex == Context.FEATURE_ENABLE_XML_SECURE_PARSING) {
+            return false;
+        }
+        return super.hasFeature(cx, featureIndex);
+    }
 }
