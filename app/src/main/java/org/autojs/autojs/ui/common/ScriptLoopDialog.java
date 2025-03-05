bc27e905aab50818f0a42cdb148f8488e46fa831
Lin <yditmxpev@mozmail.com>
2025-03-06 00:24:03 +0800

    ScriptLoopDialog 迁移到View Binding

--- app/src/main/java/org/autojs/autojs/ui/common/ScriptLoopDialog.java
+++ app/src/main/java/org/autojs/autojs/ui/common/ScriptLoopDialog.java
@@ -2,6 +2,8 @@ package org.autojs.autojs.ui.common;
 
 import android.content.Context;
 import androidx.annotation.NonNull;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
@@ -12,49 +14,37 @@ import com.stardust.app.DialogUtils;
 import com.stardust.app.GlobalAppContext;
 
 import org.autojs.autojs.R;
+import org.autojs.autojs.databinding.DialogScriptLoopBinding;
 import org.autojs.autojs.model.script.ScriptFile;
 import org.autojs.autojs.model.script.Scripts;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-/**
- * Created by Stardust on 2017/7/8.
- */
 
 public class ScriptLoopDialog {
 
     private ScriptFile mScriptFile;
     private MaterialDialog mDialog;
-
-    @BindView(R.id.loop_times)
-    EditText mLoopTimes;
-
-    @BindView(R.id.loop_interval)
-    EditText mLoopInterval;
-
-    @BindView(R.id.loop_delay)
-    EditText mLoopDelay;
-
+    private DialogScriptLoopBinding binding;
 
     public ScriptLoopDialog(Context context, ScriptFile file) {
         mScriptFile = file;
-        View view = View.inflate(context, R.layout.dialog_script_loop, null);
+        binding = DialogScriptLoopBinding.inflate(LayoutInflater.from(context));
+
         mDialog = new MaterialDialog.Builder(context)
                 .title(R.string.text_run_repeatedly)
-                .customView(view, true)
+                .customView(binding.getRoot(), true)
                 .positiveText(R.string.ok)
                 .onPositive((dialog, which) -> startScriptRunningLoop())
                 .build();
-        ButterKnife.bind(this, view);
     }
 
     private void startScriptRunningLoop() {
         try {
-            int loopTimes = Integer.parseInt(mLoopTimes.getText().toString());
-            float loopInterval = Float.parseFloat(mLoopInterval.getText().toString());
-            float loopDelay = Float.parseFloat(mLoopDelay.getText().toString());
-            Scripts.INSTANCE.runRepeatedly(mScriptFile, loopTimes, (long) (1000L * loopDelay), (long) (loopInterval * 1000L));
+            int loopTimes = Integer.parseInt(binding.loopTimes.getText().toString());
+            float loopInterval = Float.parseFloat(binding.loopInterval.getText().toString());
+            float loopDelay = Float.parseFloat(binding.loopDelay.getText().toString());
+            Scripts.INSTANCE.runRepeatedly(mScriptFile,
+                    loopTimes,
+                    (long) (1000L * loopDelay),
+                    (long) (loopInterval * 1000L));
         } catch (NumberFormatException e) {
             GlobalAppContext.toast(R.string.text_number_format_error);
         }
@@ -71,5 +61,4 @@ public class ScriptLoopDialog {
     public void show() {
         DialogUtils.showDialog(mDialog);
     }
-
 }
