b004e2dce34cbf8ae7c2592a93f0f33306c0bdd0
Lin <yditmxpev@mozmail.com>
2025-04-17 16:59:48 +0800

    禁用XML安全解析特性

--- autojs/src/main/java/com/stardust/autojs/rhino/InterruptibleAndroidContextFactory.kt
+++ autojs/src/main/java/com/stardust/autojs/rhino/InterruptibleAndroidContextFactory.kt
@@ -1,55 +1,45 @@
-package com.stardust.autojs.rhino;
+package com.stardust.autojs.rhino
 
-import android.os.Looper;
-import android.util.Log;
+import android.os.Looper
+import android.util.Log
+import com.stardust.autojs.runtime.exception.ScriptInterruptedException
+import org.mozilla.javascript.Context
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
 
-import com.stardust.autojs.runtime.exception.ScriptInterruptedException;
+/**
+ * Create a new factory. It will cache generated code in the given directory
+ *
+ * @param cacheDirectory the cache directory
+ */
+class InterruptibleAndroidContextFactory(
+    cacheDirectory: File
+) : AndroidContextFactory(cacheDirectory) {
 
-import org.mozilla.javascript.Context;
+    private val mContextCount = AtomicInteger()
+    private val LOG_TAG = "ContextFactory"
 
-import java.io.File;
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class InterruptibleAndroidContextFactory extends AndroidContextFactory {
-
-    private AtomicInteger mContextCount = new AtomicInteger();
-    private static final String LOG_TAG = "ContextFactory";
-
-    /**
-     * Create a new factory. It will cache generated code in the given directory
-     *
-     * @param cacheDirectory the cache directory
-     */
-    public InterruptibleAndroidContextFactory(File cacheDirectory) {
-        super(cacheDirectory);
-    }
-
-    @Override
-    protected void observeInstructionCount(Context cx, int instructionCount) {
-        if (Thread.currentThread().isInterrupted() && Looper.myLooper() != Looper.getMainLooper()) {
-            throw new ScriptInterruptedException();
+    override fun observeInstructionCount(cx: Context, instructionCount: Int) {
+        if (Thread.currentThread().isInterrupted && Looper.myLooper() != Looper.getMainLooper()) {
+            throw ScriptInterruptedException()
         }
     }
 
-    @Override
-    protected Context makeContext() {
-        Context cx = new AutoJsContext(this);
-        cx.setInstructionObserverThreshold(10000);
-        return cx;
+    override fun makeContext(): Context {
+        val cx = AutoJsContext(this)
+        cx.setInstructionObserverThreshold(10000)
+        return cx
     }
 
-    @Override
-    protected void onContextCreated(Context cx) {
-        super.onContextCreated(cx);
-        int i = mContextCount.incrementAndGet();
-        Log.d(LOG_TAG, "onContextCreated: count = " + i);
+    override fun onContextCreated(cx: Context) {
+        super.onContextCreated(cx)
+        val i = mContextCount.incrementAndGet()
+        Log.d(LOG_TAG, "onContextCreated: count = $i")
     }
 
-    @Override
-    protected void onContextReleased(Context cx) {
-        super.onContextReleased(cx);
-        int i = mContextCount.decrementAndGet();
-        Log.d(LOG_TAG, "onContextReleased: count = " + i);
+    override fun onContextReleased(cx: Context) {
+        super.onContextReleased(cx)
+        val i = mContextCount.decrementAndGet()
+        Log.d(LOG_TAG, "onContextReleased: count = $i")
     }
-
 }
