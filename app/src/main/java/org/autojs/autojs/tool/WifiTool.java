01809620dcaad8414613303e73941fe3c30e79dc
Lin <LYS86@users.noreply.github.com>
2025-05-16 19:38:02 +0800

    侧边栏 优化

--- app/src/main/java/org/autojs/autojs/tool/WifiTool.java
+++ app/src/main/java/org/autojs/autojs/tool/WifiTool.java
@@ -1,37 +1,40 @@
 package org.autojs.autojs.tool;
 
+import static android.content.Context.WIFI_SERVICE;
+
 import android.content.Context;
 import android.net.DhcpInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.text.format.Formatter;
-
-import java.net.InetAddress;
-
-import static android.content.Context.WIFI_SERVICE;
 
 /**
  * Created by Stardust on 2017/5/11.
  */
 
 public class WifiTool {
-
     public static String getWifiAddress(Context context) {
         WifiManager wifiMgr = (WifiManager) context.getApplicationContext().getSystemService(WIFI_SERVICE);
-        if(wifiMgr == null){
+        if (wifiMgr == null) {
             return null;
         }
         WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
         int ip = wifiInfo.getIpAddress();
-        return Formatter.formatIpAddress(ip);
+        return intToIpAddress(ip);
     }
 
-    public static String getRouterIp(Context context){
+    public static String getRouterIp(Context context) {
         WifiManager wifiService = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if(wifiService == null){
+        if (wifiService == null) {
             return null;
         }
         DhcpInfo dhcpInfo = wifiService.getDhcpInfo();
-        return Formatter.formatIpAddress(dhcpInfo.gateway);
+        return intToIpAddress(dhcpInfo.gateway);
+    }
+
+    private static String intToIpAddress(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                ((ip >> 24) & 0xFF);
     }
 }
