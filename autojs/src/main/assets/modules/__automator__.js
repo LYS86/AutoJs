b1af14de4a60aa9c05950257f5a35f1bfc91942d
Lin <LYS86@users.noreply.github.com>
2025-09-22 04:44:23 +0800

    修复press调用

--- autojs/src/main/assets/modules/__automator__.js
+++ autojs/src/main/assets/modules/__automator__.js
@@ -1,5 +1,5 @@
 
-module.exports = function(runtime, global){
+module.exports = function (runtime, global) {
     var automator = {};
 
     function performAction(action, args) {
@@ -25,76 +25,76 @@ module.exports = function(runtime, global){
 
     automator.click = function () { return performAction(runtime.automator.click.bind(runtime.automator), arguments); }
     automator.longClick = function () { return performAction(runtime.automator.longClick.bind(runtime.automator), arguments); }
-    automator.press = function () { return performAction(runtime.automator.press.bind(runtime.automator), arguments); }
-     automator.gesture = runtime.automator.gesture.bind(runtime.automator, 0);
-     automator.gestureAsync = runtime.automator.gestureAsync.bind(runtime.automator, 0);
-     automator.swipe = runtime.automator.swipe.bind(runtime.automator);
-     automator.gestures  = function(){
+    automator.press = function () { return runtime.automator.press.apply(runtime.automator, arguments); }
+    automator.gesture = runtime.automator.gesture.bind(runtime.automator, 0);
+    automator.gestureAsync = runtime.automator.gestureAsync.bind(runtime.automator, 0);
+    automator.swipe = runtime.automator.swipe.bind(runtime.automator);
+    automator.gestures = function () {
         return runtime.automator.gestures(toStrokes(arguments));
-     }
+    }
 
-     automator.gesturesAsync = function(){
-         runtime.automator.gesturesAsync(toStrokes(arguments));
-     }
+    automator.gesturesAsync = function () {
+        runtime.automator.gesturesAsync(toStrokes(arguments));
+    }
 
-     function toStrokes(args){
+    function toStrokes(args) {
         var screenMetrics = runtime.getScreenMetrics();
         var len = args.length;
         var strokes = java.lang.reflect.Array.newInstance(android.accessibilityservice.GestureDescription.StrokeDescription, len);
-        for(var i = 0; i < len; i++){
+        for (var i = 0; i < len; i++) {
             var gesture = args[i];
             var pointsIndex = 1;
-            if(typeof(gesture[1]) == 'number'){
+            if (typeof (gesture[1]) == 'number') {
                 var start = gesture[0];
                 var delay = gesture[1];
                 pointsIndex = 2;
-            }else{
+            } else {
                 var start = 0;
                 var delay = gesture[0];
             }
             var gestureLen = gesture.length;
             var path = new android.graphics.Path();
             path.moveTo(screenMetrics.scaleX(gesture[pointsIndex][0]), screenMetrics.scaleY(gesture[pointsIndex][1]));
-            for(var j = pointsIndex + 1; j < gestureLen; j++){
+            for (var j = pointsIndex + 1; j < gestureLen; j++) {
                 path.lineTo(screenMetrics.scaleX(gesture[j][0]), screenMetrics.scaleY(gesture[j][1]));
             }
             strokes[i] = new android.accessibilityservice.GestureDescription.StrokeDescription(path, start, delay);
         }
         return strokes;
-     }
+    }
 
-    automator.scrollDown = function(a, b, c, d){
-        if(arguments.length == 0)
+    automator.scrollDown = function (a, b, c, d) {
+        if (arguments.length == 0)
             return runtime.automator.scrollMaxForward();
-        if(arguments.length == 1 && typeof a === 'number')
+        if (arguments.length == 1 && typeof a === 'number')
             return runtime.automator.scrollForward(a);
-        return performAction(function(target){
+        return performAction(function (target) {
             return runtime.automator.scrollForward(target);
         }, arguments);
     }
 
-    automator.scrollUp = function(a, b, c, d){
-         if(arguments.length == 0)
+    automator.scrollUp = function (a, b, c, d) {
+        if (arguments.length == 0)
             return runtime.automator.scrollMaxBackward();
-         if(arguments.length == 1 && typeof a === 'number')
+        if (arguments.length == 1 && typeof a === 'number')
             return runtime.automator.scrollBackward(a);
-          return performAction(function(target){
+        return performAction(function (target) {
             return runtime.automator.scrollBackward(target);
         }, arguments);
     }
 
-    automator.setText = function(a, b){
-        if(arguments.length == 1){
+    automator.setText = function (a, b) {
+        if (arguments.length == 1) {
             return runtime.automator.setText(runtime.automator.editable(-1), a);
-        }else{
+        } else {
             return runtime.automator.setText(runtime.automator.editable(a), b);
         }
     }
 
-    automator.input = function(a, b){
-        if(arguments.length == 1){
+    automator.input = function (a, b) {
+        if (arguments.length == 1) {
             return runtime.automator.appendText(runtime.automator.editable(-1), a);
-        }else{
+        } else {
             return runtime.automator.appendText(runtime.automator.editable(a), b);
         }
     }
@@ -110,41 +110,41 @@ module.exports = function(runtime, global){
         "useShell": 4
     };
 
-    var auto = function(mode){
-        if(mode){
+    var auto = function (mode) {
+        if (mode) {
             global.auto.setMode(mode);
         }
         runtime.accessibilityBridge.ensureServiceEnabled();
     }
 
-    auto.waitFor = function(){
+    auto.waitFor = function () {
         runtime.accessibilityBridge.waitForServiceEnabled();
     }
 
-    auto.setMode = function(modeStr){
-        if(typeof(modeStr) !== "string"){
+    auto.setMode = function (modeStr) {
+        if (typeof (modeStr) !== "string") {
             throw new TypeError("mode should be a string");
         }
         let mode = modes[modeStr];
-        if(mode == undefined){
+        if (mode == undefined) {
             throw new Error("unknown mode for auto.setMode(): " + modeStr)
         }
         runtime.accessibilityBridge.setMode(mode);
     }
 
-    auto.setFlags = function(flags){
+    auto.setFlags = function (flags) {
         let flagStrings;
-        if(Array.isArray(flags)){
+        if (Array.isArray(flags)) {
             flagStrings = flags;
-        } else if(typeof(flags) == "string"){
+        } else if (typeof (flags) == "string") {
             flagStrings = [flags];
         } else {
             throw new TypeError("flags = " + flags);
         }
         let flagsInt = 0;
-        for(let i = 0; i < flagStrings.length; i++){
+        for (let i = 0; i < flagStrings.length; i++) {
             let flag = flagsMap[flagStrings[i]];
-            if(flag == undefined){
+            if (flag == undefined) {
                 throw new Error("unknown flag for auto.setFlags(): " + flagStrings[i]);
             }
             flagsInt |= flag;
@@ -152,38 +152,38 @@ module.exports = function(runtime, global){
         runtime.accessibilityBridge.setFlags(flagsInt);
     }
 
-    auto.__defineGetter__("service", function() {
+    auto.__defineGetter__("service", function () {
         return runtime.accessibilityBridge.getService();
     });
 
-    auto.__defineGetter__("windows", function() {
+    auto.__defineGetter__("windows", function () {
         var service = auto.service;
         return service == null ? [] : util.java.toJsArray(service.getWindows(), true);
     });
 
-    auto.__defineGetter__("root", function() {
+    auto.__defineGetter__("root", function () {
         var root = runtime.accessibilityBridge.getRootInCurrentWindow();
-        if(root == null){
+        if (root == null) {
             return null;
         }
         return com.stardust.automator.UiObject.Companion.createRoot(root);
     });
 
-    auto.__defineGetter__("rootInActiveWindow", function() {
+    auto.__defineGetter__("rootInActiveWindow", function () {
         var root = runtime.accessibilityBridge.getRootInActiveWindow();
-        if(root == null){
+        if (root == null) {
             return null;
         }
         return com.stardust.automator.UiObject.Companion.createRoot(root);
     });
 
-    auto.__defineGetter__("windowRoots", function() {
+    auto.__defineGetter__("windowRoots", function () {
         return util.java.toJsArray(runtime.accessibilityBridge.windowRoots(), false)
             .map(root => com.stardust.automator.UiObject.Companion.createRoot(root));
     });
 
 
-    auto.setWindowFilter = function(filter){
+    auto.setWindowFilter = function (filter) {
         runtime.accessibilityBridge.setWindowFilter(new com.stardust.autojs.core.accessibility.AccessibilityBridge.WindowFilter(filter));
     }
 
