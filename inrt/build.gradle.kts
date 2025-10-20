4239931b603d503e8c6c5a5f45495a373530a761
Lin <106499400+LYS86@users.noreply.github.com>
2025-10-20 11:59:04 +0800

    chore(deps): 更新依赖配置和插件引用方式

--- inrt/build.gradle.kts
+++ inrt/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 kotlin {
     jvmToolchain(21)
 }
 android {
-    // 使用 buildSrc 中的 Versions 对象
     compileSdk = Versions.compileSdk
 
     defaultConfig {
@@ -95,6 +94,7 @@ tasks.whenTaskAdded {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(libs.appcompat)
     androidTestImplementation(libs.espresso.core) {
         exclude(group = "com.android.support", module = "support-annotations")
     }
