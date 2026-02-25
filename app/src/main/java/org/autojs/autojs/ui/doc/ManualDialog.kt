f25953d0eaf329cd208ba2f340548fd31c03e361
Lin <106499400+LYS86@users.noreply.github.com>
2026-02-26 04:31:54 +0800

    refactor: 迁移 ManualDialog 到 ViewBinding

---
+++ app/src/main/java/org/autojs/autojs/ui/doc/ManualDialog.kt
@@ -0,0 +1,54 @@
+package org.autojs.autojs.ui.doc
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.afollestad.materialdialogs.MaterialDialog
+import org.autojs.autojs.databinding.FloatingManualDialogBinding
+
+class ManualDialog(context: Context) {
+
+    private val binding = FloatingManualDialogBinding.inflate(
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as android.view.LayoutInflater
+    )
+    private val dialog: Dialog = MaterialDialog.Builder(context)
+        .customView(binding.root, false)
+        .build()
+        .apply {
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+
+    init {
+        binding.close.setOnClickListener { dialog.dismiss() }
+        binding.fullscreen.setOnClickListener {
+            dialog.dismiss()
+            DocumentationActivity_.intent(binding.root.context)
+                .extra(DocumentationActivity.EXTRA_URL, binding.ewebView.webView.url)
+                .start()
+        }
+    }
+
+    fun title(title: String): ManualDialog {
+        binding.title.text = title
+        return this
+    }
+
+    fun url(url: String): ManualDialog {
+        binding.ewebView.webView.loadUrl(url)
+        return this
+    }
+
+    fun pinToLeft(listener: android.view.View.OnClickListener): ManualDialog {
+        binding.pinToLeft.setOnClickListener {
+            dialog.dismiss()
+            listener.onClick(it)
+        }
+        return this
+    }
+
+    fun show(): ManualDialog {
+        dialog.show()
+        return this
+    }
+}
