f8c1f7819a12bf0a8cec26467b3c8c8fb4a23ac1
Lin <106499400+LYS86@users.noreply.github.com>
2026-02-28 04:51:35 +0800

    refactor: 迁移 EditActivity 到 ViewBinding

---
+++ app/src/main/java/org/autojs/autojs/ui/edit/EditActivity.kt
@@ -0,0 +1,294 @@
+package org.autojs.autojs.ui.edit
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import com.stardust.app.OnActivityResultDelegate
+import com.stardust.autojs.core.permission.OnRequestPermissionsResultCallback
+import com.stardust.autojs.core.permission.PermissionRequestProxyActivity
+import com.stardust.autojs.core.permission.RequestPermissionCallbacks
+import com.stardust.autojs.execution.ScriptExecution
+import com.stardust.pio.PFiles
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.autojs.autojs.R
+import org.autojs.autojs.databinding.ActivityEditBinding
+import org.autojs.autojs.storage.file.TmpScriptFiles
+import org.autojs.autojs.theme.dialog.ThemeColorMaterialDialogBuilder
+import org.autojs.autojs.tool.Observers
+import org.autojs.autojs.ui.BaseActivity
+import org.autojs.autojs.ui.main.MainActivity
+import java.io.File
+import java.io.IOException
+import androidx.core.view.get
+import androidx.core.view.size
+
+class EditActivity : BaseActivity(),
+    OnActivityResultDelegate.DelegateHost,
+    PermissionRequestProxyActivity {
+
+    private lateinit var binding: ActivityEditBinding
+    private val mediator = OnActivityResultDelegate.Mediator()
+    private val requestPermissionCallbacks = RequestPermissionCallbacks()
+    private var editorMenu: EditorMenu? = null
+    private var newTask = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        newTask = (intent.flags and Intent.FLAG_ACTIVITY_NEW_TASK) != 0
+        setUpViews()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun setUpViews() {
+        binding.editorView.handleIntent(intent)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(Observers.emptyConsumer()) { ex ->
+                onLoadFileError(ex.message)
+            }
+        editorMenu = EditorMenu(binding.editorView)
+        setUpToolbar()
+    }
+
+    override fun onWindowStartingActionMode(callback: ActionMode.Callback): ActionMode? {
+        return super.onWindowStartingActionMode(callback)
+    }
+
+    override fun onWindowStartingActionMode(callback: ActionMode.Callback, type: Int): ActionMode? {
+        return super.onWindowStartingActionMode(callback, type)
+    }
+
+    private fun onLoadFileError(message: String?) {
+        ThemeColorMaterialDialogBuilder(this)
+            .title(getString(R.string.text_cannot_read_file))
+            .content(message ?: "")
+            .positiveText(R.string.text_exit)
+            .cancelable(false)
+            .onPositive { _, _ -> finish() }
+            .show()
+    }
+
+    private fun setUpToolbar() {
+        setToolbarAsBack(this, R.id.toolbar, binding.editorView.name)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_editor, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return editorMenu?.onOptionsItemSelected(item) ?: false
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        Log.d(LOG_TAG, "onPrepareOptionsMenu: $menu")
+        val isScriptRunning = binding.editorView.scriptExecutionId != ScriptExecution.NO_ID
+        val forceStopItem = menu.findItem(R.id.action_force_stop)
+        forceStopItem.isEnabled = isScriptRunning
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onActionModeStarted(mode: ActionMode) {
+        Log.d(LOG_TAG, "onActionModeStarted: $mode")
+        val menu = mode.menu
+        val item = menu[menu.size - 1]
+        menu.add(item.groupId, R.id.action_delete_line, 10000, R.string.text_delete_line)
+        menu.add(item.groupId, R.id.action_copy_line, 20000, R.string.text_copy_line)
+        super.onActionModeStarted(mode)
+    }
+
+    override fun onSupportActionModeStarted(mode: androidx.appcompat.view.ActionMode) {
+        Log.d(LOG_TAG, "onSupportActionModeStarted: mode = $mode")
+        super.onSupportActionModeStarted(mode)
+    }
+
+    override fun onWindowStartingSupportActionMode(callback: androidx.appcompat.view.ActionMode.Callback): androidx.appcompat.view.ActionMode? {
+        Log.d(LOG_TAG, "onWindowStartingSupportActionMode: callback = $callback")
+        return super.onWindowStartingSupportActionMode(callback)
+    }
+
+    override fun startActionMode(callback: ActionMode.Callback, type: Int): ActionMode? {
+        Log.d(LOG_TAG, "startActionMode: callback = $callback, type = $type")
+        return super.startActionMode(callback, type)
+    }
+
+    override fun startActionMode(callback: ActionMode.Callback): ActionMode? {
+        Log.d(LOG_TAG, "startActionMode: callback = $callback")
+        return super.startActionMode(callback)
+    }
+
+    override fun onBackPressed() {
+        if (binding.editorView.onBackPressed().not()) {
+            super.onBackPressed()
+        }
+    }
+
+    override fun finish() {
+        if (binding.editorView.isTextChanged) {
+            showExitConfirmDialog()
+            return
+        }
+        finishAndRemoveFromRecents()
+    }
+
+    private fun finishAndRemoveFromRecents() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAndRemoveTask()
+        } else {
+            super.finish()
+        }
+        if (newTask) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun showExitConfirmDialog() {
+        ThemeColorMaterialDialogBuilder(this)
+            .title(R.string.text_alert)
+            .content(R.string.edit_exit_without_save_warn)
+            .positiveText(R.string.text_cancel)
+            .negativeText(R.string.text_save_and_exit)
+            .neutralText(R.string.text_exit_directly)
+            .onNegative { _, _ ->
+                binding.editorView.saveFile()
+                finishAndRemoveFromRecents()
+            }
+            .onNeutral { _, _ -> finishAndRemoveFromRecents() }
+            .show()
+    }
+
+    override fun onDestroy() {
+        binding.editorView.destroy()
+        super.onDestroy()
+    }
+
+    override fun getOnActivityResultDelegateMediator(): OnActivityResultDelegate.Mediator {
+        return mediator
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        mediator.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (binding.editorView.isTextChanged.not()) {
+            return
+        }
+        val text = binding.editorView.editor.text
+        if (text.length < 256 * 1024) {
+            outState.putString(KEY_TEXT, text)
+        } else {
+            val tmp = saveToTmpFile(text)
+            if (tmp != null) {
+                outState.putString(KEY_PATH, tmp.path)
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun saveToTmpFile(text: String): File? {
+        return try {
+            val tmp = TmpScriptFiles.create(this)
+            Observable.just(text)
+                .observeOn(Schedulers.io())
+                .subscribe { t -> PFiles.write(tmp, t) }
+            tmp
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val text = savedInstanceState.getString(KEY_TEXT)
+        if (text != null) {
+            binding.editorView.setRestoredText(text)
+            return
+        }
+        val path = savedInstanceState.getString(KEY_PATH)
+        if (path != null) {
+            Observable.just(path)
+                .observeOn(Schedulers.io())
+                .map { PFiles.read(it) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { t -> binding.editorView.editor.text = t },
+                    { it.printStackTrace() }
+                )
+        }
+    }
+
+    override fun addRequestPermissionsCallback(callback: OnRequestPermissionsResultCallback) {
+        requestPermissionCallbacks.addCallback(callback)
+    }
+
+    override fun removeRequestPermissionsCallback(callback: OnRequestPermissionsResultCallback): Boolean {
+        return requestPermissionCallbacks.removeCallback(callback)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        requestPermissionCallbacks.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val LOG_TAG = "EditActivity"
+        private const val KEY_TEXT = "text"
+        private const val KEY_PATH = "path"
+
+        @JvmStatic
+        fun editFile(context: Context, path: String, newTask: Boolean) {
+            editFile(context, null, path, newTask)
+        }
+
+        @JvmStatic
+        fun editFile(context: Context, uri: Uri, newTask: Boolean) {
+            context.startActivity(newIntent(context, newTask).setData(uri))
+        }
+
+        @JvmStatic
+        fun editFile(context: Context, name: String?, path: String, newTask: Boolean) {
+            context.startActivity(
+                newIntent(context, newTask)
+                    .putExtra(EditorView.EXTRA_PATH, path)
+                    .putExtra(EditorView.EXTRA_NAME, name)
+            )
+        }
+
+        fun viewContent(context: Context, name: String, content: String, newTask: Boolean) {
+            context.startActivity(
+                newIntent(context, newTask)
+                    .putExtra(EditorView.EXTRA_CONTENT, content)
+                    .putExtra(EditorView.EXTRA_NAME, name)
+                    .putExtra(EditorView.EXTRA_READ_ONLY, true)
+            )
+        }
+
+        private fun newIntent(context: Context, newTask: Boolean): Intent {
+            val intent = Intent(context, EditActivity::class.java)
+            if (newTask || context !is Activity) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            return intent
+        }
+    }
+}
